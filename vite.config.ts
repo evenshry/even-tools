ae68dbccaa2c6f894d6323f823c67ad34918cb56
shiyuan <syuan@366ec.com>
2026-02-28 17:54:13 +0800

    根据环境模式来确定资源路径的配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  root: './',
-  base: process.env.BASE_URL || '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'react-vendor'
-            }
-            if (id.includes('antd') || id.includes('@ant-design')) {
-              return 'antd'
-            }
-          }
-          if (id.includes('unitData')) {
-            return 'unit-data'
-          }
-        }
-      }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    root: "./",
+    base: mode == "deploy" ? "/even-tools/" : "./",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-    chunkSizeWarningLimit: 600
-  }
-})
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              if (id.includes("react") || id.includes("react-dom") || id.includes("react-router-dom")) {
+                return "react-vendor";
+              }
+              if (id.includes("antd") || id.includes("@ant-design")) {
+                return "antd";
+              }
+            }
+            if (id.includes("unitData")) {
+              return "unit-data";
+            }
+          },
+        },
+      },
+      chunkSizeWarningLimit: 600,
+    },
+  };
+});
